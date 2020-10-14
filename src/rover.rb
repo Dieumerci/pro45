77db32c0a189063dd8c391938a5de2b4538f9a82
Dieumerci <dieumerci.dev.kaz@gmail.com>
2020-10-14 21:05:18 +0200

    created the rspec folders

--- src/rover.rb
+++ src/rover.rb
@@ -1,6 +1,5 @@
 class Rover
   attr_accessor :x_coordinate, :y_coordinate, :direction
-
   def initialize(x_coordinate, y_coordinate, direction)
     @x_coordinate = x_coordinate
     @y_coordinate = y_coordinate
